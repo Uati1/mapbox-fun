7692dd580a4bbbdfccecbb63a6d5e044f7f376e8
szymon latacz <szymon.latacz@mmc.pl>
2022-03-02 16:22:21 +0100

    static map onload, unspaghetting code

---
+++ pages/components/windowDimension/index.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const getWindowDimensions = () => {
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+  else return {height:100,width: 100}
+}
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+export default useWindowDimensions;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState,useMemo,memo} from 'react';
 import  mapboxgl from 'mapbox-gl';
 import generator from './components/generator';
 import { usePosition } from 'use-position';
 import polygon from './components/polygon'
 import 'mapbox-gl/dist/mapbox-gl.css'; 
-import icon from './assets/pin.png'
+import useWindowDimensions from './components/windowDimension'
 
 
 const Home = ()=>{
   //split to tiles
-  //memo tiles with layers
+  //memo if geoJSON unchanged
   // fire full load on drag, unzooom only map + button load events, on specific zoom
   //add types
   interface pointObject {
@@ -25,53 +25,22 @@ const Home = ()=>{
     type: string,
     features: Array<featureObject>
   }
-  mapboxgl.accessToken = 'pk.eyJ1IjoibmVvc2NhIiwiYSI6ImNrZm80ZnI4MzJlcHoyeW52eGZqeDVpNXcifQ.0b4R6NcNKL9SNDq1q7ECrA';
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic2xhdGFjeiIsImEiOiJja24yNnFsNmgwY3gzMnFtbjNxODR2Z2Z6In0.L1xJBH_-hTpQ4QCRoNCDCQ';
   const mapRef = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null);
   const [points,setPoints] = useState<geoJSONObject | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const { latitude, longitude, error }:{latitude: number, longitude: number, error: string} = usePosition();
-  const [start, setStart] = useState({
+  const [render, setRender] = useState(0);
+  const initialState = {
     ln: 19.145136,
     lt: 51.919438,
     zoom: 5
-  })
-  useEffect(()=>{
-    setPoints(generator(100000));
-  },[])
-  useEffect(()=>{
-    if(map.current=== null) return;
-    const marker1 = new mapboxgl.Marker()
-      .setLngLat([start.ln,start.lt])
-      .addTo(map.current);
-    map.current.flyTo({
-      center: [start.ln,start.lt],
-      zoom: start.zoom
-    });
-  },[start,map]);
-  useEffect(()=>{
-    if (error || !latitude) return; 
-    if (latitude && longitude && !error) {
-      setStart((prev)=>({
-        ...prev,
-        ln: longitude,
-        lt: latitude,
-        zoom: 10
-      }))
-    }
-  },[longitude,latitude,error])
-  
-  useEffect(()=>{
-    if(map.current!== null) return;
-    map.current = new mapboxgl.Map({
-      container: mapRef.current,
-      "type": "vector",
-      style: 'mapbox://styles/mapbox/light-v10?optimize=true',
-      center: [start.ln, start.lt],
-      zoom: start.zoom,
-      minzoom: 4,
-      maxzoom: 10
-    });
+  }
+  //delete previous markers if there are any
+  const [start, setStart] = useState(initialState)
+  const addMapEventListeners = ()=>{
+    //single event
     map.current.on('click', 'unclustered-point', (e:any) => {
       const coordinates = e.features[0].geometry.coordinates.slice();
 
@@ -83,13 +52,14 @@ const Home = ()=>{
         .setLngLat(coordinates)
         .setHTML(`<div><h2>${e.features[0].properties.sport}</h2><div>${e.features[0].properties.host}</div></div>`)
         .addTo(map.current);
+    });
+    //cluster
+    map.current.on('click', 'clusters', (e:any) => {
+      const features = map.current.queryRenderedFeatures(e.point, {
+        layers: ['clusters']
       });
-      map.current.on('click', 'clusters', (e:any) => {
-        const features = map.current.queryRenderedFeatures(e.point, {
-          layers: ['clusters']
-        });
-        const clusterId = features[0].properties.cluster_id;
-        map.current.getSource('places').getClusterExpansionZoom(
+      const clusterId = features[0].properties.cluster_id;
+      map.current.getSource('places').getClusterExpansionZoom(
         clusterId,
         (err:any, zoom:any ) => {
           if (err) return;
@@ -98,106 +68,153 @@ const Home = ()=>{
             zoom: zoom
           });
         }
-        );
-      });
-      // Change the cursor to a pointer when the mouse is over the places layer.
-      map.current.on('mouseenter', 'unclustered-point', () => {
-        map.current.getCanvas().style.cursor = 'pointer';
-      });
-       
-      // Change it back to a pointer when it leaves.
-      map.current.on('mouseleave', 'unclustered-point', () => {
-        map.current.getCanvas().style.cursor = '';
-      });
-      map.current.on('sourcedata', () => {
-        console.log('A sourcedata event occurred.');
-        });
-  },[])
-  useEffect(()=>{
-    if(points===null) return;
-    console.log(points)
-    if (points&& map.current!==null&&loading===false){
-      setLoading(true)
+      );
+    });
+    //mouseenter & mouseleave
+    map.current.on('mouseenter', 'unclustered-point', () => {
+      map.current.getCanvas().style.cursor = 'pointer';
+    });
       
-      map.current.on('load', ()=>{
-        console.log('A load event occurred.');
-        map.current.loadImage(
-          'https://i.ibb.co/3p77qkp/pin.png',
-          (error:any, image:any) => {
-            if (error) throw error;
-             
-            // Add the image to the map style.
-            map.current.addImage('pin', image);
+    map.current.on('mouseleave', 'unclustered-point', () => {
+      map.current.getCanvas().style.cursor = '';
+    });
+    //map loaded
+    map.current.on('idle', () => {
+      setLoading(false);
+    });
+  }
 
-          map.current.addSource('places', {
-            type: 'geojson',
-            data: points,
-            cluster: true,
-            clusterMaxZoom: 13, // Max zoom to cluster points on
-            clusterRadius: 100,
-            tolerance: 1
-          })
-          map.current.addLayer({
-            id: 'clusters',
-            type: 'circle',
-            source: 'places',
-            filter: ['has', 'point_count'],
-            paint: {
-                // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-                // with three steps to implement three types of circles:
-                //   * Blue, 20px circles when point count is less than 100
-                //   * Yellow, 30px circles when point count is between 100 and 750
-                //   * Pink, 40px circles when point count is greater than or equal to 750
-                'circle-color': [
-                  'step',
-                  ['get', 'point_count'],
-                  '#51bbd6',
-                  100,
-                  '#f1f075',
-                  750,
-                  '#f28cb1'
-                ],
-                'circle-radius': [
-                  'step',
-                  ['get', 'point_count'],
-                  20,
-                  100,
-                  30,
-                  750,
-                  40
-                ]
-              }
-          });
-             
-          map.current.addLayer({
-            id: 'cluster-count',
-            type: 'symbol',
-            source: 'places',
-            filter: ['has', 'point_count'],
-            layout: {
-              'text-field': '{point_count_abbreviated}',
-              'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-              'text-size': 12
-            }
-          });
-             
-          map.current.addLayer({
-            id: 'unclustered-point',
-            type: 'symbol',
-            source: 'places',
-            layout: {
-              'icon-image': 'pin',
-              'icon-allow-overlap': true,
-              'icon-size': 0.05
-              },
-            filter: ['!', ['has', 'point_count']],
-           
-          });
-        })
+  const addLayersOnLoad = () =>{
+    if(map.current ===null) return
+    map.current.on('load', ()=>{
+      console.log('loading image')
+      // addImage
+      map.current.loadImage(
+        'https://i.ibb.co/3p77qkp/pin.png',
+        (error:any, image:any) => {
+          if (error) console.log(error) ;
+          map.current.addImage('pin', image);
+      });
+      //add GeoJSON as source
+      map.current.addSource('places', {
+        type: 'geojson',
+        data: points,
+        cluster: true,
+        clusterMaxZoom: 13, 
+        clusterRadius: 100,
+        tolerance: 1
       })
+
+      //cluster layer
+      map.current.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'places',
+        filter: ['has', 'point_count'],
+        paint: {
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#51bbd6',
+              100,
+              '#f1f075',
+              750,
+              '#f28cb1'
+            ],
+            'circle-radius': [
+              'step',
+              ['get', 'point_count'],
+              20,
+              100,
+              30,
+              750,
+              40
+            ]
+          }
+      });
+      // cluster count
+      map.current.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'places',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
+      });
+      // single event
+      map.current.addLayer({
+        id: 'unclustered-point',
+        type: 'symbol',
+        source: 'places',
+        layout: {
+          'icon-image': 'pin',
+          'icon-allow-overlap': true,
+          'icon-size': 0.05
+          },
+        filter: ['!', ['has', 'point_count']],
+      });
+      
+    })
+  }
+  const generateMap = ()=>{
+    setLoading(true);
+      map.current = new mapboxgl.Map({
+      container: mapRef.current,
+      type: "vector",
+      style: 'mapbox://styles/slatacz/cl09oziqt000x15s1igs277dx',
+      center: [start.ln, start.lt],
+      zoom: start.zoom,
+      maxZoom: 15,
+      minZoom: 6
+    });
+  }
+  useEffect(()=>{
+    setPoints(generator(100000));
+  },[])
+
+  useEffect( ()=>{
+    if(loading === true) return;
+    if(map.current=== null) return;
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat([start.ln,start.lt])
+      .addTo(map.current);
+    map.current.flyTo({
+      center: [start.ln,start.lt],
+      zoom: start.zoom
+    });
+  },[loading])
+  useEffect(()=>{
+    if (error || !latitude) return; 
+    if (latitude && longitude && !error) {
+      setStart((prev)=>({
+        ...prev,
+        ln: longitude,
+        lt: latitude,
+        zoom: 10
+      }))
     }
-  },[points])
-  return <div className="container" ref = {mapRef}></div>;
+  },[longitude,latitude,error])
+  useEffect(()=>{
+    if(points === null) return
+    generateMap();
+    addMapEventListeners();
+    addLayersOnLoad();
+  },[render,points])
+
+  return <>
+    <div id='static' className={loading?'':'hide'}><img src={`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${initialState.ln},${initialState.lt-6},${initialState.zoom}/720x1280?access_token=${mapboxgl.accessToken}`} /></div>
+    <div className={loading?'hide container':'container'} ref = {mapRef}></div>
+    <button onClick={()=>setPoints(generator(100000))}>generate</button>
+    <button onClick={()=>setRender(prev=>prev+1)}>render</button>
+  </>;
 }
 
 export default Home
